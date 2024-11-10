ec7594c58eb27dce4ac2d44e2f3a2ebeb652803f
Abdurrahman Abuzaid <abdurrahman.abzd@gmail.com>
2024-11-10 23:01:05 +0300

    Add reading thermistor function

--- Cooling_FireDetection_System/sensor.cpp
+++ Cooling_FireDetection_System/sensor.cpp
@@ -1,16 +1,6 @@
 #include "sensor.h"
 #include <DHT.h>
 
-#define DHTPIN1 2
-#define DHTPIN2 3
-#define DHTTYPE DHT11
-#define UVSensorPin A0
-#define FlameSensorPin 4
-#define COSensorPin A1
-
-DHT dht1(DHTPIN1, DHTTYPE);
-DHT dht2(DHTPIN2, DHTTYPE);
-
 float temperature1;
 float temperature2;
 int uvValue;
@@ -18,11 +8,16 @@ int flameDetected;
 int coLevel;
 
 void initializeSensors() {
-  dht1.begin();
-  dht2.begin();
-  pinMode(UVSensorPin, INPUT);
-  pinMode(FlameSensorPin, INPUT);
-  pinMode(COSensorPin, INPUT);
+  //Initialize Thermistor
+  Serial.begin(9600);
+  pinMode(THERMO1, INPUT);
+  pinMode(THERMO2, INPUT);
+  pinMode(THERMO3, INPUT);
+
+  
+  pinMode(UVSensorPin, INPUT); //Initialize UV Sensor
+  pinMode(FlameSensorPin, INPUT); //Initialize Flame Detection Sensor
+  pinMode(COSensorPin, INPUT); //Initialize Gas Detection Sensor
 }
 
 void readSensors() {
@@ -32,3 +27,27 @@ void readSensors() {
   flameDetected = digitalRead(FlameSensorPin);
   coLevel = analogRead(COSensorPin);
 }
+
+float readTemp(int thermistorPin) { // source : sensor datasheet example code
+  int analogValue = analogRead(thermistorPin); // Reading the analog input from the thermistor (temperature sensor).
+  float resistance = (1024.0 / analogValue - 1.0) * 10000.0; // Calculate the thermistor resistance (assuming a 10k pull-down resistor)
+
+  // Convert resistance to temperature using the Steinhard-Hart equation
+  float temperature = 1.0 / (0.001129148 + 0.000234125 * log(resistance) + 0.0000000876741 * pow(log(resistance), 3)); 
+  temperature -= 273.15; // Convert Kelvin to Celcius
+  
+  return temperature;
+}
+
+int readUV() {
+
+}
+
+void detectFlame() {
+
+}
+
+void detectGas() {
+
+}
+
--- Cooling_FireDetection_System/sensor.h
+++ Cooling_FireDetection_System/sensor.h
@@ -1,10 +1,22 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
-#include <DHT.h>
+// Define I/O pin numbers
+#define THERMO1 A0 // 3 x Thermistor KY-013 
+#define THERMO2 A1
+#define THERMO2 A2
+#define UVSENSOR A3 //3 x GUVA-S12SD
+#define GASSENSOR A4 // 1 x Gas Sensor MQ-2
+#define FLAMESENSOR 4 // 2 x flame detection sensor
 
 void initializeSensors();
 void readSensors();
+
+float readTemp(int thermistorPin);
+int readUV();
+void detectGas();
+void detectFlame();
+
 
 extern float temperature1;
 extern float temperature2;
